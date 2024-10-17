c3543f62f47d7e66cb9af5ad9b375b55c83a5fc5
Nilafhiosagam <45123924+Nilafhiosagam@users.noreply.github.com>
2024-10-17 17:04:42 +0100

    add calc derivative to helper

--- helper_functions.py
+++ helper_functions.py
@@ -1,3 +1,6 @@
+from scipy.signal import savgol_filter
+import numpy as np
+
 def normalize_signal(signal):
     """
     Normalize the signal to the range [0, 1].
@@ -21,3 +24,12 @@ def normalize_signal(signal):
     normalize_signal(df['Signal'].values)
     """
     return (signal - signal.min()) / (signal.max() - signal.min())
+
+
+
+def calculate_derivative(volume, signal):
+    # Smooth the signal first to reduce noise
+    signal_smooth = savgol_filter(signal, window_length=11, polyorder=3)
+    # Calculate the derivative
+    dydx = np.gradient(signal_smooth, volume)
+    return dydx
